63adf6cb13899351d9032fed9b8ca6af1fffebdf
Jaxofw <jaxofw@gmail.com>
2024-04-05 20:49:31 -0400

    fix: change footer height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   const { discord, twitter } = Config.Links
 
   return (
-    <div className="flex h-[168px] flex-col items-center justify-center bg-footer text-white">
+    <div className="flex h-1/6 flex-col items-center justify-center bg-footer text-white">
       <h1 className="text-lg font-semibold">Follow Us</h1>
       <div className="flex space-x-8 pt-4 text-xl">
         <button
