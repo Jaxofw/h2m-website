cad87eb44d29f346a9f22012d8ae15e8e494c7eb
Jaxofw <jaxofw@gmail.com>
2024-04-05 21:27:46 -0400

    feat: add responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { faDiscord } from '@fortawesome/free-brands-svg-icons'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { useLocation, useNavigate } from 'react-router-dom'
@@ -24,13 +26,19 @@ const items = [
 
 const Navbar = () => {
   const { discord } = Config.Links
-
   const { pathname } = useLocation()
   const navigate = useNavigate()
 
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleMenuClick = (path: string) => {
+    setMenuOpen(false)
+    navigate(path)
+  }
+
   return (
-    <nav className="flex h-[100px] w-full items-center justify-between bg-transparent px-5 outline outline-1 outline-white/25">
-      <div className="flex space-x-4">
+    <nav className="flex h-[100px] w-full items-center justify-around bg-transparent px-5 outline outline-1 outline-white/25 lg:justify-between">
+      <div className="hidden space-x-4 lg:flex">
         {items.map(({ label, path }) => (
           <button
             key={label}
@@ -44,7 +52,7 @@ const Navbar = () => {
         ))}
       </div>
       <Logo width={160} />
-      <div className="flex h-full items-end space-x-4">
+      <div className="hidden h-full items-end space-x-4 lg:flex">
         <Button
           icon={faDiscord}
           className="h-11 rounded-b-none rounded-t-xl bg-button-secondary px-6 hover:bg-button-hover"
@@ -60,6 +68,41 @@ const Navbar = () => {
           <h1 className="font-heebo text-md">PLAY NOW</h1>
         </Button>
       </div>
+      <button
+        className="absolute right-0 mr-10 rounded-md bg-secondary p-2 lg:hidden"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+      </button>
+      {menuOpen && (
+        <div className="absolute top-[6.25rem] z-50 w-full bg-black lg:hidden">
+          <ul className="text-center text-white">
+            {items.map(({ label, path }) => (
+              <li key={label}>
+                <button
+                  onClick={() => handleMenuClick(path)}
+                  className="font-heebo w-full border-b-2 py-2 text-lg"
+                >
+                  {label}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   )
 }
