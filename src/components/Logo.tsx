69057d877123255a2da0da5a56294a4342c92180
Jaxofw <jaxofw@gmail.com>
2024-04-05 21:36:35 -0400

    fix: add home re-route on logo click

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,13 @@
+import { useNavigate } from 'react-router-dom'
+
 import H2MLogo from '@/assets/logo.png'
 
 export default function Logo({ width }: { width: number }) {
-  return <img src={H2MLogo} alt="logo" width={width} />
+  const navigate = useNavigate()
+
+  return (
+    <button onClick={() => navigate('/')}>
+      <img src={H2MLogo} alt="logo" width={width} />
+    </button>
+  )
 }
